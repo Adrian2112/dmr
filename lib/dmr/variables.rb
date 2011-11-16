82a47bceb748ba55f2d382c4bc31901272c89018
Adrian Gonzalez <bernardogzzf@gmail.com>
2011-11-16 16:41:00 -0600

    se agrego funion de boolean que regresa el boolean de un string o boolean

--- lib/dmr/variables.rb
+++ lib/dmr/variables.rb
@@ -18,6 +18,23 @@ module DMR
     CONSTANTE_INT           = 15000..15999
     CONSTANTE_FLOAT         = 16000..16999
     
+    attr_reader :globales_int
+    attr_reader :globales_float
+    attr_reader :globales_boolean
+    attr_reader :globales_string
+    attr_reader :locales_int
+    attr_reader :locales_float
+    attr_reader :locales_boolean
+    attr_reader :locales_string
+    attr_reader :temporales_locales_int
+    attr_reader :temporales_locales_float
+    attr_reader :temporales_locales_boolean
+    attr_reader :temporales_globales_int
+    attr_reader :temporales_globales_float
+    attr_reader :temporales_globales_boolean
+    attr_reader :constante_int
+    attr_reader :constante_float
+    
     def initialize
       @globales_int = {}
       @globales_float = {}
@@ -44,18 +61,18 @@ module DMR
       case direccion
         when GLOBAL_INT                 then @globales_int.merge!({direccion => valor.to_i})
         when GLOBAL_FLOAT               then @globales_float.merge!({direccion => valor.to_f})
-        when GLOBAL_BOOLEAN             then @globales_boolean.merge!({direccion => valor.to_boolean})
+        when GLOBAL_BOOLEAN             then @globales_boolean.merge!({direccion => boolean(valor)})
         when GLOBAL_STRING              then @globales_string.merge!({direccion => valor.gsub("\"", "")})
         when LOCAL_INT                  then @locales_int.merge!({direccion => valor.to_i})
         when LOCAL_FLOAT                then @locales_float.merge!({direccion => valor.to_f})
-        when LOCAL_BOOLEAN              then @locales_boolean.merge!({direccion => valor.to_boolean})
+        when LOCAL_BOOLEAN              then @locales_boolean.merge!({direccion => boolean(valor)})
         when LOCAL_STRING               then @locales_string.merge!({direccion => valor.gsub("\"", "")})
         when TEMPORAL_GLOBAL_INT        then @temporales_globales_int.merge!({direccion => valor.to_i})
         when TEMPORAL_GLOBAL_FLOAT      then @temporales_globales_float.merge!({direccion => valor.to_f})
-        when TEMPORAL_GLOBAL_BOOLEAN    then @temporales_globales_boolean.merge!({direccion => valor.to_boolean})
+        when TEMPORAL_GLOBAL_BOOLEAN    then @temporales_globales_boolean.merge!({direccion => boolean(valor)})
         when TEMPORAL_LOCAL_INT         then @temporales_locales_int.merge!({direccion => valor.to_i})
         when TEMPORAL_LOCAL_FLOAT       then @temporales_locales_float.merge!({direccion => valor.to_f})
-        when TEMPORAL_LOCAL_BOOLEAN     then @temporales_locales_boolean.merge!({direccion => valor.to_boolean})
+        when TEMPORAL_LOCAL_BOOLEAN     then @temporales_locales_boolean.merge!({direccion => boolean(valor)})
         when CONSTANTE_INT              then @constante_int.merge!({direccion => valor.to_i})
         when CONSTANTE_FLOAT            then @constante_float.merge!({direccion => valor.to_f})
       end
@@ -81,6 +98,15 @@ module DMR
         when CONSTANTE_INT              then return @constante_int[direccion]
         when CONSTANTE_FLOAT            then return @constante_float[direccion]
       end
+    end
+    
+    def boolean(valor)
+      if valor.class == String
+        valor.to_boolean
+      elsif valor.class == TrueClass or valor.class == FalseClass
+        return valor
+      end
+      return false
     end
             
   end
